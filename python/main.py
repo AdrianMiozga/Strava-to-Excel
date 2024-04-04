104c9debc830a2875109c5f64911148dcb1804e9
Adrian Miozga <AdrianMiozga@outlook.com>
2024-04-04 22:42:12 +0200

    Add OAuth token refreshing

--- python/main.py
+++ python/main.py
@@ -5,6 +5,8 @@ from datetime import datetime
 
 from requests_oauthlib import OAuth2Session
 
+PERSISTENCE_FILE = "python/persistence.json"
+
 
 def main():
     with open("python/config.json", "r", encoding="UTF-8") as file:
@@ -13,26 +15,53 @@ def main():
         client_id = contents.get("client_id")
         client_secret = contents.get("client_secret")
 
-    # TODO: Implement refreshing token
+    token = None
+
+    try:
+        with open(PERSISTENCE_FILE, "r", encoding="UTF-8") as file:
+            contents = json.load(file)
+
+            token = {
+                "access_token": contents.get("access_token"),
+                "refresh_token": contents.get("refresh_token"),
+                "token_type": "Bearer",
+                "expires_in": contents.get("expires_at") - datetime.now().timestamp(),
+            }
+    except FileNotFoundError:
+        pass
+
+    extra = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+    }
+
     oauth = OAuth2Session(
         client_id,
+        token=token,
         redirect_uri="https://localhost/exchange_token",
+        auto_refresh_url="https://www.strava.com/api/v3/oauth/token",
+        auto_refresh_kwargs=extra,
         scope=["read,activity:read_all"],
+        token_updater=token_saver,
     )
 
-    authorization_url, _ = oauth.authorization_url(
-        "https://www.strava.com/oauth/authorize"
-    )
+    if not token or not token.get("access_token"):
+        authorization_url, _ = oauth.authorization_url(
+            "https://www.strava.com/oauth/authorize"
+        )
 
-    webbrowser.open(authorization_url)
-    authorization_response = input("Enter the full callback URL: ")
+        webbrowser.open(authorization_url)
 
-    oauth.fetch_token(
-        "https://www.strava.com/api/v3/oauth/token",
-        authorization_response=authorization_response,
-        client_secret=client_secret,
-        include_client_id=True,
-    )
+        authorization_response = input("Enter the full callback URL: ")
+
+        token = oauth.fetch_token(
+            "https://www.strava.com/api/v3/oauth/token",
+            authorization_response=authorization_response,
+            client_secret=client_secret,
+            include_client_id=True,
+        )
+
+        token_saver(token)
 
     time = datetime(int(datetime.now().strftime("%Y")), 1, 1, 0, 0, 0)
     timestamp = calendar.timegm(time.timetuple())
@@ -73,6 +102,18 @@ def main():
         file.write(str(round(meters / 1000)))
 
     print(f"Data updated. New distance {round(meters / 1000)} km")
+
+
+def token_saver(token):
+    with open(PERSISTENCE_FILE, "w", encoding="UTF-8") as file:
+        data = {
+            "access_token": token.get("access_token"),
+            "refresh_token": token.get("refresh_token"),
+            "expires_at": token.get("expires_at"),
+        }
+
+        json.dump(data, file, indent=2)
+        file.write("\n")
 
 
 if __name__ == "__main__":
